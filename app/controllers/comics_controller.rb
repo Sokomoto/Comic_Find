7e753913ade3373c2ce0185dbe1eca1072500204
Sokomoto <wdws2342319@gmail.com>
2020-07-21 11:48:14 +0900

    投稿機能1

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,17 +1,23 @@
 class ComicsController < ApplicationController
+	before_action :correct_user, only: [:edit,:update]
+
+	def new
+		@comic = Comic.new
+	end
 
 	def create
+		@comic = Comic.new(comic_params)
+		@comic.user_id = current_user.id
+		@comic.save
+		redirect_to comics_path
 	end
 
 	def index
     	@comics = Comic.all
 	end
 
-	def new
-		@comic = Comic.new
-	end
-
 	def show
+		@comic = Comic.find(params[:id])
 	end
 
 	def edit
@@ -26,6 +32,13 @@ class ComicsController < ApplicationController
 	private
 
 	def comic_params
-  	params.require(:comic).permit(:title, :explanation)
-  end
+  	params.require(:comic).permit(:image, :title, :explanation)
+  	end
+
+  	def correct_user
+    @comic = current_user.comics.find_by(id: params[:id])
+      unless @comic
+      redirect_to comics_path
+      end
+  	end
 end
