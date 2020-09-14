822b13c0dc5b75819cc3028adc1c7caea39f9e60
Sokomoto <wdws2342319@gmail.com>
2020-09-14 14:07:33 +0900

    レビュー編集機能3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,42 @@
 class UsersController < ApplicationController
+	before_action :correct_user
 
 	def show
-		@user = User.find(params[:id])
 	end
 
 	def edit
 	end
 
 	def update
-	end
+		if @user.update(user_params)
+    		redirect_to user_path(@user)
+    	else
+      		render :edit
+    	end
+  	end
 
 	def leave
 	end
 
 	def hide
+		if @user.update(is_status: "退会済")
+			sign_out current_user
+			redirect_to root_path, notice: "退会処理が完了しました。ご利用ありがとうございました。"
+    	else
+      		render :edit
+    	end
 	end
 
 	private
 
 	def user_params
-    params.require(:user).permit(:name, :profile_image, :email)
+    	params.require(:user).permit(:name, :profile_image, :email)
+  	end
+
+  	def correct_user
+	    @user = User.find(params[:id])
+	    unless current_user.id == @user.id
+	      redirect_to user_path(current_user)
+	    end
   	end
 end
