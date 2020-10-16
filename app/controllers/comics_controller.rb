2e3adf6ad23864a1134d3eb045635061c6219fcb
Sokomoto <wdws2342319@gmail.com>
2020-10-17 02:55:05 +0900

    user側の全体的なレイアウト調整

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -15,9 +15,8 @@ class ComicsController < ApplicationController
 	end
 
 	def index
-		@tag_list = Tag.all
     	@comics = Comic.all
-    	@all_ranks = Comic.find(Favorite.group(:comic_id).order('count(comic_id) desc').limit(5).pluck(:comic_id))
+		@all_ranks = Comic.find(Favorite.group(:comic_id).order('count(comic_id) desc').limit(5).pluck(:comic_id))
 	end
 
 	def show
